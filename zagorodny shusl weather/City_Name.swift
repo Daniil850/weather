0b914774a16880ec6863101e6eba87b7aa12e1de
Daniil850 <daniilzagorodny029@gmail.com>
2021-12-14 20:50:52 +0300

    1

---
+++ zagorodny shusl weather/City_Name.swift
@@ -0,0 +1,25 @@
+//
+//  City_name.swift
+//  zagorodny shusl weather
+//
+//  Created by Даниил on 14.12.2021.
+//
+import UIKit
+
+class City_Name: UITableViewCell {
+
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityTemp: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
